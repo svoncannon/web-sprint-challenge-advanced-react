116a593c65f69168100e9c45be39dd929c9194ce
Samantha VonCannon <66282132+svoncannon@users.noreply.github.com>
2021-01-15 12:31:25 -0500

    finished MVP and stretch

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,76 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event';
+import ShoppingCart from "./ShoppingCart";
 
 // Write up the two tests here and make sure they are testing what the title shows
+test("form header renders", async() => {
+    const { getByText } = render(<CheckoutForm />);
+    //isolate the h2 by text
+    const header = getByText(/checkout form/i); 
+    //expect it to be in the document 
+    expect(header).toBeInTheDocument();
+});
 
-test("form header renders", () => {});
+test("form shows success message on submit with form details", async() => {
+    // grab rendered component 
+    const { getByLabelText, getByRole, getByTestId } = render(<CheckoutForm />);
+    
+    // get the different inputs 
 
-test("form shows success message on submit with form details", () => {});
+    const firstNameInput = getByLabelText(/first name/i); 
+    await userEvent.type(firstNameInput, 'samantha');
+    expect(firstNameInput).toHaveValue('samantha'); 
+
+    const lastNameInput = getByLabelText(/last name/i); 
+    await userEvent.type(lastNameInput, 'voncannon');
+    expect(lastNameInput).toHaveValue('voncannon'); 
+
+    const addressInput = getByLabelText(/address/i); 
+    await userEvent.type(addressInput, '123 happy lane');
+    expect(addressInput).toHaveValue('123 happy lane'); 
+
+    const cityInput = getByLabelText(/city/i); 
+    await userEvent.type(cityInput, 'phoenix');
+    expect(cityInput).toHaveValue('phoenix');
+    
+    const stateInput = getByLabelText(/state/i); 
+    await userEvent.type(stateInput, 'az');
+    expect(stateInput).toHaveValue('az');
+    
+    const zipInput = getByLabelText(/zip/i); 
+    await userEvent.type(zipInput, '550055');
+    expect(zipInput).toHaveValue('550055'); 
+
+    // grab the button 
+    const checkoutBtn = getByRole("button", { name: /checkout/i}); 
+    // click the button 
+    await userEvent.click(checkoutBtn); 
+    expect(await getByTestId("successMessage"));
+});
+
+const plants = [
+    {id: 1,
+    name: 'Peperomia Rosso',
+    price: 21,
+    img: 'data.plantsData[0].img'
+    },
+    {id: 2,
+    name: 'Snake Plant', 
+    price: 18,
+    img: 'data.plantsData[0].img'
+    }, 
+    {id: 3,
+    name: 'ZZ Plant', 
+    price: 18,
+    img: 'data.plantsData[0].img'
+    }]; 
+
+test("Shopping cart shows selected plants", async() => {
+    const { getByTestId } = render(<ShoppingCart cart={plants} />);
+    
+    // get plant deets 
+   const plant = getByTestId('plant-name');
+   expect(plant).toBeInTheDocument();
+});
