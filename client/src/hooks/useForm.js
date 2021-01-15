aa91d3f5e9aa9b77a10a4b95b405858522181798
Samantha VonCannon <66282132+svoncannon@users.noreply.github.com>
2021-01-15 12:02:06 -0500

    finished hook and plantlist

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,19 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+export const useForm = (initialValue) => {
+    //set form values into state
+    const [formValues, setValues] = useState(initialValue);
+
+    //handle changes
+    const handleChanges = e => {
+        const { name, value } = e.target;
+        setValues({
+            ...formValues,
+            [name]: value
+        });
+
+    };
+    //return array
+    return [formValues, handleChanges]
+}
